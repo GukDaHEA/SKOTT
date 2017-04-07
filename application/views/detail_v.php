f3acc98378463dc8b117b5ac0d1340c0c6554458
HwAnijj <zziltongjh@gmail.com>
2017-04-07 20:42:34 +0900

    detail 수정

--- application/views/detail_v.php
+++ application/views/detail_v.php
@@ -146,9 +146,10 @@
            </div>
       <div id="div_Taxi" style="position: fixed; background-color: #FFF; margin-left: 1000px; z-index: 1000;">
          <h3 style="color:#f3753a;font-weight:">택시 정보 </h3><br/>
-
+<!-- 
       <iframe src="/index.php/topic/navi" width="600" height="300">
-      </iframe>
+      </iframe> -->
+
 <!-- <div id="div_Address" style="text-align:center">
         출발지 : <input type="text" style="color:blue; font-size:15px;width:250px;height:30px" readonly /> <br />
         도착지 : <input type="text" style="color:blue; font-size:15px;width:250px;height:30px" value="서울특별시 종로구 사직로 161" readonly /> <br />
@@ -156,109 +157,8 @@
       <div id = "div_Map">
         지도부분
       </div>
-      <script type="text/javascript" src="//apis.daum.net/maps/maps3.js?apikey=5291d1a32b8219f767e162d92e72a6ee&libraries=services,clusterer,drawing"></script>
- -->
        <script type="text/javascript">
-      //   var mapContainer = document.getElementById('div_Map'), // 지도를 표시할 div 
-      //       mapOption = {
-      //           center: new daum.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-      //           level: 10 // 지도의 확대 레벨
-      //       };  
-
-      //   // 지도를 생성합니다    
-      //   var map = new daum.maps.Map(mapContainer, mapOption); 
-
-      //   // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-      //   if (navigator.geolocation) {
-            
-      //       // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-      //       navigator.geolocation.getCurrentPosition(function(position) {
-                
-      //           var lat = position.coords.latitude, // 위도
-      //               lon = position.coords.longitude; // 경도
-                
-      //           var locPosition = new daum.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-      //               message = '<div style="padding:5px;text-align:center;">내 위치</div>'; // 인포윈도우에 표시될 내용입니다
-                
-      //           // 마커와 인포윈도우를 표시합니다
-      //           displayMarker(locPosition, message);
-                    
-      //         });
-            
-      //   } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-            
-      //       var locPosition = new daum.maps.LatLng(33.450701, 126.570667),    
-      //           message = 'geolocation을 사용할수 없어요..'
-                
-      //       displayMarker(locPosition, message);
-      //   }
-
-        // function displayMarker(locPosition, message) {
-
-        //     // 마커를 생성합니다
-        //     var marker = new daum.maps.Marker({  
-        //         map: map, 
-        //         position: locPosition
-        //     }); 
-            
-        //     var iwContent = message, // 인포윈도우에 표시할 내용
-        //         iwRemoveable = true;
-
-        //     // 인포윈도우를 생성합니다
-        //     var infowindow = new daum.maps.InfoWindow({
-        //         content : iwContent,
-        //         removable : iwRemoveable
-        //     });
-            
-        //     // 인포윈도우를 마커위에 표시합니다 
-        //     infowindow.open(map, marker);
-            
-        //     // // 지도 중심좌표를 접속위치로 변경합니다
-        //     // map.setCenter(locPosition);      
-        // }    
-
-
-        // //주소-좌표 변환 객체를 생성합니다
-        // var geocoder = new daum.maps.services.Geocoder();
- 
-        // // 주소로 좌표를 검색합니다
-        // geocoder.addr2coord('서울특별시 종로구 사직로 161', function(status, result) {
-
-        //     // 정상적으로 검색이 완료됐으면 
-        //      if (status === daum.maps.services.Status.OK) {
-
-        //         var coords = new daum.maps.LatLng(result.addr[0].lat, result.addr[0].lng);
-
-        //         // 결과값으로 받은 위치를 마커로 표시합니다
-        //         var marker = new daum.maps.Marker({
-        //             map: map,
-        //             position: coords
-        //         });
-
-        //         // 인포윈도우로 장소에 대한 설명을 표시합니다
-        //         var infowindow = new daum.maps.InfoWindow({
-        //             content: '<div style="width:150px;text-align:center;padding:6px 0;">광화문</div>'
-        //         });
-        //         infowindow.open(map, marker);
-
-        //         // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-        //         map.setCenter(coords);
-        //     } 
-        // });    
-
-      //           // 지도 타입 변경 컨트롤을 생성한다
-      //   var mapTypeControl = new daum.maps.MapTypeControl();
-      //   // 지도의 상단 우측에 지도 타입 변경 컨트롤을 추가한다
-      //   map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT); 
-      //   // 지도에 확대 축소 컨트롤을 생성한다
-      //   var zoomControl = new daum.maps.ZoomControl();
-      //   // 지도의 우측에 확대 축소 컨트롤을 추가한다
-      //   map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
-      //   function searchDetailAddrFromCoords(coords, callback) {
-      //     // 좌표로 법정동 상세 주소 정보를 요청합니다
-      //     geocoder.coord2detailaddr(coords, callback);
-      // }
-
+        
       </script>
          <!-- <div id= "div_taxiinfo">
         <ul> <li> 20분 </li>
@@ -301,27 +201,9 @@
             </ul>
         </div>
  </div>
-      <ol class="sort">
-            <li  val="date"><a href="javascript:power_review_sort('date');">최신 순</a></li>
-            <li class="now" val="score"><a href="javascript:power_review_sort('score');">평점 순</a></li>
-            <li  val="good"><a href="javascript:power_review_sort('good');">추천 순</a></li>
-        </ol>
-           <div id = "div_ReviewStar">
-
-        </div>
         <span id = "div_ReviewWrite">
         <button onclick="javascript:addBoard()">리뷰 작성</button>
       </span>
-    </div>
- 
-      
-   </header>
-  
-   <header>
-   <div id = "div_bgm">
-      <audio src="/static/image/아리랑.mp3" controls="controls" loop="loop"</audio>
-   </div>
-   </header>
 </div>
 
 
