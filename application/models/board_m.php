33c35a43561c3572893fcef76afe15383e4558e6
HwAnijj <zziltongjh@gmail.com>
2017-05-21 18:51:07 +0900

    띄어쓰기

--- application/models/board_m.php
+++ application/models/board_m.php
@@ -57,7 +57,6 @@ class Board_m extends CI_Model
 
 	function board_modify($modify_subject, $modify_contents, $id, $modify_picture) {
 
-
 		$sql = "update ci_board set subject = '".$modify_subject."', contents = '".$modify_contents."', picture = '".$modify_picture."' where board_id ='".$id."'";
 
 		$query = $this->db->query($sql);
