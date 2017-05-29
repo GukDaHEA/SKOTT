54c88b986cf40b63fd3cc8cb89dba6fdc5d8cfb2
songchanghwan <songstudy@naver.com>
2017-05-29 14:18:15 +0900

    [2017. 05. 29] 창환 mains 수정

--- application/controllers/mains.php
+++ application/controllers/mains.php
@@ -6,15 +6,15 @@ class Mains extends MY_Controller {
 	}
 
     function index(){
-        $recommand_name = $this->Mains_m->reco_name();
+        // $recommand_name = $this->Mains_m->reco_name();
 
-        $recommand_name= json_encode($recommand_name, JSON_UNESCAPED_UNICODE);
+        // $recommand_name= json_encode($recommand_name, JSON_UNESCAPED_UNICODE);
 
-        $this->load->view('header', 
-            array(  
-                'recommand_name'=>$recommand_name                               
-                )               
-        );
+        // $this->load->view('header', 
+        //     array(  
+        //         'recommand_name'=>$recommand_name                               
+        //         )               
+        // );
 
         $this->load->view('mains_v');
         $this->load->view('footer');
