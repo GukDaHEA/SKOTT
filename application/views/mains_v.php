50bdf0a878423af17745f7d87c42a3baf3d7de54
songchanghwan <songstudy@naver.com>
2017-06-04 16:10:25 +0900

    [2017. 06. 04] 경로및 이미지크기 등등 수정

--- application/views/mains_v.php
+++ application/views/mains_v.php
@@ -59,7 +59,7 @@
                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                   <span class="fa fa-bars color-white"></span>
                </button>
-               <h1><a class="navbar-brand" href="index.html" data-0="line-height:90px;" data-300="line-height:50px;"> <div class = "img_logo"></div>   
+               <h1><a class="navbar-brand" href="/mains" data-0="line-height:90px;" data-300="line-height:50px;"> <div class = "img_logo"></div>   
                </a></h1> 
             </div>
             <div class="navbar-collapse collapse">
@@ -120,10 +120,10 @@
                      <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.9s">                        
                         <form class="form-inline">
                            <div class="form-group">
-                              <button type="livedemo" name="Live Demo" class="btn btn-primary btn-lg" required="required">LIST</button>
+                              <button type="livedemo" name="Live Demo" class="btn btn-primary btn-lg" required="required">SKOTT</button>
                            </div>
                            <div class="form-group">
-                              <button type="getnow" name="Get Now" class="btn btn-primary btn-lg" required="required">START TOUR</button>
+                               <button type="getnow" name="Get Now" class="btn btn-primary btn-lg" required="required">START TOUR</button>
                            </div>
                         </form>
                      </div>
@@ -258,8 +258,8 @@
                <div class="wow bounceIn">
                   <div class="align-center">
                      <div class="wow rotateIn">
-                        <div class="service-col" style="height: 520px;">
-                           <div class="service-icon">
+                        <div class="service-col" style="height: 550px;">
+                           <div class="service-icon" style = "width : 230px; height : 230px;">
                            <img class = "img_service" src="/static/image/mains/TOP3/8.jpg">
                            </div>
                               <h2><a href="#">TOP1<br> < 이태원 > </a></h2> 
@@ -275,8 +275,8 @@
                   <div class="wow bounceIn">
                   
                      <div class="wow rotateIn">
-                        <div class="service-col"  style="height: 520px;">   
-                           <div class="service-icon">
+                        <div class="service-col"  style="height: 550px;">   
+                           <div class="service-icon" style = "width : 230px; height : 230px;">
                               <img class = "img_service" src="/static/image/mains/TOP3/4.jpg">
                            </div>
                               <h2><a href="#">TOP2<br> < 남산 > </a></h2> 
@@ -290,12 +290,12 @@
             <div class="col-lg-4" >
                <div class="align-center">
                   <div class="wow bounceIn">
-                     <div class="service-col" style="height: 520px;">
-                        <div class="service-icon">
+                     <div class="service-col" style="height: 550px;">
+                       <div class="service-icon" style = "width : 230px; height : 230px;">
                            <img class = "img_service" src="/static/image/mains/TOP3/9.jpg">
                         </div>
                            <h2><a href="#">TOP3<br> < 홍대입구 > </a></h2> 
-                           <p>홍대(弘大)는 서울특별시 마포구 서울 지하철 2호선 홍대입구역과 홍익대학교 정문을 잇는 지역 및 이 일대에 있는 번화가를 말한다. '홍대'는 홍익대학교의 한국어 약칭으로 이 지역의 정식 지명이 아닌 통용 명칭이며, 홍익대학교와 구분하기 위하여 홍대입구, 홍대거리라고도 부른다.</p>
+                           <p>홍대(弘大)는 서울특별시 마포구 서울 지하철 2호선 홍대입구역과 홍익대학교 정문을 잇는 지역 및 이 일대에 있는 번화가를 말한다. '홍대'는 홍익대학교의 한국어 약칭으로 이 지역의 정식 지명이 아닌 통용 명칭이다.</p>
                      </div>
                   </div>
                </div>
@@ -769,4 +769,5 @@
     <script src="/static/tem_contactform/contactform.js"></script>
     
 </body>
+
 </html>
